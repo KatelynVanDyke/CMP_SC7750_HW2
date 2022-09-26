ab32a049f8e05f1def58d393a3ecd51e6c1751b4
Olivia LaVal <oj.laval@gmail.com>
2022-09-25 19:56:11 -0500

    Add metrics to uniform cost tree
    Add metrics and then update report file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import copy
+import time
 
 
 class Location:
@@ -6,13 +7,20 @@ class Location:
         self.x = x
         self.y = y
 
+    def __str__(self):
+        return "(" + str(self.x) + ", " + str(self.y) + ") "
+
+    def __repr__(self):
+        return str(self)
+
 
 class Node:
-    def __init__(self, x, y, state, cost):
+    def __init__(self, x, y, state, cost, path):
         self.x = x
         self.y = y
         self.state = state
         self.cost = cost
+        self.path = path
 
 
 max_x = 4
@@ -32,17 +40,29 @@ instance2 = [['W', 'W', 'W', 'W', 'W', 'W'],
 
 # problemSpace -> a matrix of what's clean and dirty
 # initialLocation -> the location of the vaccuum
-def treeSearch(problemSpace, initialLocation) -> Node:
+def treeSearch(problemSpace, initialLocation):
     fringe = []
-    fringe = Insert(Make_Node(initialLocation.x, initialLocation.y, problemSpace, 0), fringe)
-    while True:
+    fringe = Insert(Make_Node(initialLocation.x, initialLocation.y, problemSpace, 0, [initialLocation]), fringe)
+    # variables to store metrics
+    numOfGenerated = 0
+    numOfExpanded = 0
+    # clock variable
+    clockEnd = time.time() + 600
+    while time.time() < clockEnd:
         if len(fringe) == 0:
-            return None
+            return None, numOfGenerated, numOfExpanded
         currNode = fringe.pop(0)
-        print(str(currNode.x) + ", " + str(currNode.y))
+        # print(str(currNode.x) + ", " + str(currNode.y))
         if Goal_Test(currNode.state):
-            return currNode
-        fringe = Insert_All(Expand(currNode), fringe)
+            return currNode, numOfGenerated, numOfExpanded
+        expandedNodes = Expand(currNode)
+        # metrics
+        numOfGenerated = numOfGenerated + len(expandedNodes)
+        numOfExpanded = numOfExpanded + 1
+        # insert expanded into fringe
+        fringe = Insert_All(expandedNodes, fringe)
+    # if we reach here we've timed out
+    return None, numOfGenerated, numOfExpanded
 
 
 def Insert(node, fringe):
@@ -59,29 +79,31 @@ def Insert_All(nodes, fringe):
     return fringe
 
 
-def Make_Node(x, y, state, cost) -> Node:
-    return Node(x, y, state, cost)
+def Make_Node(x, y, state, cost, path) -> Node:
+    return Node(x, y, state, cost, path)
 
 
 def Expand(node):
     nodes = []
+    path = node.path.copy()
+    path.append(Location(node.x, node.y))
     # left
     if node.y > 1:
-        nodes.append(Make_Node(node.x, node.y-1, node.state, node.cost+1))
+        nodes.append(Make_Node(node.x, node.y-1, node.state, node.cost+1, path))
     # right
     if node.y < max_y:
-        nodes.append(Make_Node(node.x, node.y+1, node.state, node.cost+.9))
+        nodes.append(Make_Node(node.x, node.y+1, node.state, node.cost+.9, path))
     # up
     if node.x > 1:
-        nodes.append(Make_Node(node.x-1, node.y, node.state, node.cost+.8))
+        nodes.append(Make_Node(node.x-1, node.y, node.state, node.cost+.8, path))
     # down
     if node.x < max_x:
-        nodes.append(Make_Node(node.x+1, node.y, node.state, node.cost+.7))
+        nodes.append(Make_Node(node.x+1, node.y, node.state, node.cost+.7, path))
     # suck
     if node.state[node.x][node.y] == 'D':
         newState = copy.deepcopy(node.state)
         newState[node.x][node.y] = 'C'
-        nodes.append(Make_Node(node.x, node.y, newState, node.cost+.6))
+        nodes.append(Make_Node(node.x, node.y, newState, node.cost+.6, path))
 
     return nodes
 
@@ -95,14 +117,48 @@ def Goal_Test(problemSpace) -> bool:
     return True
 
 
+# run instance 1
+# record the start time
+startTime = time.time()
+# run the algorithm
 problemSpace1 = copy.deepcopy(instance1)
-result1 = treeSearch(problemSpace1, Location(2, 2))
-print("\nresult: " + str(result1.x) + " " + str(result1.y) + " " + str(result1.cost))
-print("Problem Space:")
-for i in range(max_x+1):
-    for j in range(max_y+1):
-        print(result1.state[i][j], end='')
-    print('')
+result1, generated, expanded = treeSearch(problemSpace1, Location(2, 2))
+# record the stop time
+endTime = time.time()
 
-# result2 = treeSearch(instance2, Location(3, 2))
-# print(str(result2.x) + " " + str(result2.y) + " " + str(result2.cost))
+if result1 is not None:
+    print("\nresult: " + str(result1.x) + " " + str(result1.y) + " " + str(result1.cost))
+    print("Problem Space:")
+    for i in range(max_x+1):
+        for j in range(max_y+1):
+            print(result1.state[i][j], end='')
+        print('')
+    for elem in result1.path:
+        print(elem, end=', ')
+else:
+    print("\nresult: No goal state found")
+
+print("Time taken: %.5f" % (endTime-startTime))
+print("Generated: " + str(generated) + ", Expanded: " + str(expanded) + "\n\n")
+
+# run instance 2
+# record the start time
+startTime = time.time()
+# run the algorithm
+problemSpace2 = copy.deepcopy(instance2)
+result2, generated, expanded = treeSearch(problemSpace2, Location(3, 2))
+# record the stop time
+endTime = time.time()
+
+if result2 is not None:
+    print("\nresult: " + str(result2.x) + " " + str(result2.y) + " " + str(result2.cost))
+    print("Problem Space:")
+    for i in range(max_x+1):
+        for j in range(max_y+1):
+            print(result2.state[i][j], end='')
+        print('')
+else:
+    print("\nresult: No goal state found")
+
+print("Time taken: %.5f" % (endTime-startTime))
+print("Generated: " + str(generated) + ", Expanded: " + str(expanded) + "\n\n")
